dc5242ab46bbd1c30ff7f1b50430b5ae8cd0bae3
Snehangsu De <desnehangsu@gmail.com>
2022-08-10 01:16:25 +0530

    Add subtraction

--- src/mathlib.py
+++ src/mathlib.py
@@ -1,8 +1,9 @@
 # Basic function
 def addition(num1, num2):
-    print(f"\nThe sum of {num1} and {num2} is {num1 + num2}.\n")
     return num1 + num2
 
+def subtraction(num1, num2):
+    return num1 - num2
 
 if __name__ == "__main__":
     addition(8, 10)
--- tests/test_math.py
+++ tests/test_math.py
@@ -6,3 +6,6 @@ def test_addition():
     output = mathlib.addition(2,4)
     assert output == 6, "Test Passed"
  
+def test_subtraction():
+    output = mathlib.subtraction(5, 2)
+    assert output == 3, "Test Passed"

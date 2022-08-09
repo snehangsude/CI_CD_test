3bbbb9f6d85ee2d964a47dce2c33ce4e55b53653
Snehangsu De <desnehangsu@gmail.com>
2022-08-10 01:12:42 +0530

    Add subtraction

--- src/mathlib.py
+++ src/mathlib.py
@@ -3,6 +3,10 @@ def addition(num1, num2):
     print(f"\nThe sum of {num1} and {num2} is {num1 + num2}.\n")
     return num1 + num2
 
+def subtraction(num1, num2):
+    print(f"\nThe difference in {num1} and {num2} is {num1 - num2}.\n")
+    return num1 - num2
+
 
 if __name__ == "__main__":
     addition(8, 10)
